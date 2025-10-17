b53ca76d4e273e36b1c13f394dfaa9bd0281f201
Sakil Ahmed <luahmedsakil403@gmail.com>
2025-10-17 22:49:52 +0600

    Create Q_1.dart

---
+++ Assignment 02/Practice 02/Q_1.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Enter a number: ');
+  int num = int.parse(stdin.readLineSync()!);
+
+  if (num % 2 == 0) {
+    print('$num is even');
+  }
+  else {
+    print('$num is odd');
+  }
+}
