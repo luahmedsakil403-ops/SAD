d7bfc96658a023d9ee66222f218fc2f8ba47c9c2
Sakil Ahmed <luahmedsakil403@gmail.com>
2025-10-21 22:54:20 +0600

    Update Q_1.dart

--- Practice 04/Q_1.dart
+++ Practice 04/Q_1.dart
@@ -1,6 +1,6 @@
 // Create a list of names and print all names using list.
 void main() {
-    List<String>s =['mahmudulsakib2019','ecnarwala','tourist','Fahmi_nafi_46'];
+    List<String>s =['sakilahmed2019','ecnarwala','tourist','Fahmi_nafi_46'];
     for(var i in s) {
         print(i);
     }
