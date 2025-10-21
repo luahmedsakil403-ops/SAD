054537892be0692757de506bd05469240504edfc
Sakil Ahmed <luahmedsakil403@gmail.com>
2025-10-21 22:52:20 +0600

    Create Q_1.dart

---
+++ Practice 04/Q_1.dart
@@ -0,0 +1,7 @@
+// Create a list of names and print all names using list.
+void main() {
+    List<String>s =['mahmudulsakib2019','ecnarwala','tourist','Fahmi_nafi_46'];
+    for(var i in s) {
+        print(i);
+    }
+}
