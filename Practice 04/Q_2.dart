c9907b10a6826b52f777f7554393675601c86a0d
Sakil Ahmed <luahmedsakil403@gmail.com>
2025-10-21 22:55:29 +0600

    Create Q_2.dart

---
+++ Practice 04/Q_2.dart
@@ -0,0 +1,7 @@
+// Create a set of fruits and print all fruits using loop.
+void main() {
+    Set<String>s = {'Apple','Banana','Carrot','Tangerin'};
+    for(var i in s) {
+        print(i);
+    }
+}
